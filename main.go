57a4f1ca3e4a9978a5ba33c661e1b7a02f8ee357
Haakon Refsvik <hakon.refsvik@me.com>
2026-03-10 13:40:14 +0100

    removed secret key from codebase

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -13,7 +15,6 @@ import (
 const (
 	DATABASE   = "/tmp/minitwit.db"
 	PER_PAGE   = 30
-	SECRET_KEY = "development key"
 )
 
 // Globals
@@ -21,6 +22,7 @@ var (
 	db           *gorm.DB
 	store        *DBStore
 	sessionStore *sessions.CookieStore
+    SECRET_KEY   = os.Getenv("SECRET_KEY")
 )
 
 // Router setup
@@ -62,6 +64,7 @@ func setupRouter() *mux.Router {
 }
 
 func main() {
+    godotenv.Load() // loads .env automatically
 	initDB()
 	store = NewDBStore(db)
 	sessionStore = newStore()
