89ced5a516a3b1030ad71644eb68efdb811b0a3d
Haakon Refsvik <hakon.refsvik@me.com>
2026-04-20 12:28:00 +0200

    added connection pool

--- db.go
+++ db.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func initDB() {
-	var err error
-	dsn := getSecretOrEnv("DATABASE_URL")
+    var err error
+    dsn := getSecretOrEnv("DATABASE_URL")
 
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	err = db.AutoMigrate(&User{}, &Message{}, &Follower{})
-	if err != nil {
-		log.Fatal("Failed to migrate database:", err)
-	}
+    // Add this block:
+    sqlDB, err := db.DB()
+    if err != nil {
+        log.Fatal(err)
+    }
+    sqlDB.SetMaxOpenConns(25)
+    sqlDB.SetMaxIdleConns(10)
+    sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+    err = db.AutoMigrate(&User{}, &Message{}, &Follower{})
+    if err != nil {
+        log.Fatal("Failed to migrate database:", err)
+    }
 }
