20cc540af6a5ab1fa2a369db965e7be8485b313a
Haakon Refsvik <hakon.refsvik@me.com>
2026-04-20 13:45:53 +0200

    go formatting

--- db.go
+++ db.go
@@ -9,25 +9,25 @@ import (
 )
 
 func initDB() {
-    var err error
-    dsn := getSecretOrEnv("DATABASE_URL")
+	var err error
+	dsn := getSecretOrEnv("DATABASE_URL")
 
-    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal(err)
-    }
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Add this block:
-    sqlDB, err := db.DB()
-    if err != nil {
-        log.Fatal(err)
-    }
-    sqlDB.SetMaxOpenConns(25)
-    sqlDB.SetMaxIdleConns(10)
-    sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// Add this block:
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlDB.SetMaxOpenConns(25)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
 
-    err = db.AutoMigrate(&User{}, &Message{}, &Follower{})
-    if err != nil {
-        log.Fatal("Failed to migrate database:", err)
-    }
+	err = db.AutoMigrate(&User{}, &Message{}, &Follower{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
 }
